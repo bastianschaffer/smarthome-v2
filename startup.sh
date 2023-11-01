16915329085713d02b7eab97b898ce7c58a08a3f
bastianschaffer <bastianschaffer.it@gmail.com>
2023-11-01 22:33:23 +0000

    Add Venv and Edit Startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd ~/Documents/smarthome-v2/
-python server/server.py & cd client && npm start
+start_server() {
+	# . /home/pi/.../activate is the equivalent to source /home/pi/.../activate for sh	
+	cd /home/pi/Documents/smarthome-v2/ && . /home/pi/Documents/smarthome-v2/server/venv/bin/activate && python server/server.py
+    
+}
+
+start_npm() {
+    cd /home/pi/Documents/smarthome-v2/client && npm start 
+}
+
+echo starting up smarthome application...
+sleep 30
+start_server &
+start_npm &
